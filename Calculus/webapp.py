302493af1643f488fce4faa7836e0e04f48943e6
WEN Hao <wenh06@gmail.com>
2023-11-01 22:50:11 +0800

    add admin functions for calculus webapp

--- Calculus/webapp.py
+++ Calculus/webapp.py
@@ -19,6 +19,8 @@ def load_table():
     password_table["密码"] = password_table["密码"].apply(lambda p: eval(p))  # string of bytes to bytes
     table = table.join(password_table.set_index("学生用户名"))
     table = table.fillna(-1)
+    # drop columns with all -1 values
+    table = table.drop(columns=[c for c in table.columns if all(table[c] == -1)])
     for col in table.columns:
         if col not in ["学生姓名", "密码", "所属分组", "班级"]:
             # convert from float to int
@@ -28,16 +30,20 @@ def load_table():
 
 table = load_table()
 
+
+st.title("2023秋微积分查询随堂测验成绩")
+
 name = st.text_input("姓名", help="请输入姓名", key="name")
 student_id = st.text_input("学号", help="请输入学号", key="student_id")
 
 password = st.text_input("密码", help="请输入密码", key="password", type="password")
 
+magic_name = "郭德纲"
+magic_student_id = 110
+magic_password = b"$2b$12$uWlKeOBuUFoLLFSyt0qZ2.rpbjk1dbWHG/AT3qT9heTEwa4hiPgWG"
+
 
 def consult():
-    magic_name = "郭德纲"
-    magic_student_id = 110
-    magic_password = b"$2b$12$uWlKeOBuUFoLLFSyt0qZ2.rpbjk1dbWHG/AT3qT9heTEwa4hiPgWG"
     if name == "" or student_id == "":
         st.error("姓名和学号不能为空")
         return
@@ -85,6 +91,50 @@ def consult():
 # add a button to call function consult
 
 st.button("查询", on_click=consult)
+
+
+# sidebar for admin
+
+if "is_admin" not in st.session_state:
+    st.session_state["is_admin"] = 0
+
+st.sidebar.markdown("**管理员功能**")
+
+if not st.session_state.is_admin:
+    admin_password = st.sidebar.text_input("管理员密码", help="请输入管理员密码", key="admin_password", type="password")
+    admin_login_button = st.sidebar.button("管理员登录", help="管理员登录")
+
+    if admin_login_button:
+        if bcrypt.checkpw(admin_password.encode("utf-8"), magic_password):
+            st.sidebar.success("登录成功")
+            st.session_state.is_admin = 1
+            st.rerun()
+        else:
+            st.sidebar.error("密码错误")
+
+# if bcrypt.checkpw(st.session_state["admin_password"].encode("utf-8"), magic_password):
+if st.session_state.is_admin:
+    # logout button
+    logout_button = st.sidebar.button("管理员登出", help="管理员登出")
+    if logout_button:
+        st.sidebar.success("登出成功")
+        # st.session_state["is_admin"] = False
+        st.session_state.is_admin = False
+        # reload the page
+        st.rerun()
+    else:
+        # reload table button
+        reload_button = st.sidebar.button("重新加载表格", help="重新加载表格")
+        if reload_button:
+            # clear cache
+            st.cache_resource.clear()
+            # reload table
+            table = load_table()
+            st.success("表格已重新加载")
+        # show the whole table
+        show_table_button = st.sidebar.button("显示所有学生成绩", help="显示所有学生成绩")
+        if show_table_button:
+            st.table(table[[c for c in table.columns if c not in ["密码", "所属分组"]]])
 
 # command to run:
 # nohup streamlit run webapp.py --server.port 8501 > .logs/webapp.log 2>&1 & echo $! > .logs/webapp.pid
