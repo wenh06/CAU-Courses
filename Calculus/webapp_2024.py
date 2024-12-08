6ee791e79d4693b39c8cdbd0602f517d4c924479
WEN Hao <wenh06@gmail.com>
2024-12-08 11:26:08 +0800

    fix bugs

--- Calculus/webapp_2024.py
+++ Calculus/webapp_2024.py
@@ -76,6 +76,29 @@ def consult():
         st.write(f"缺交第{', '.join(map(str, indices + 1))}次作业")
 
 
+def compute_final_exam_score(contest_ratio, final_exam_ratio):
+    if student_id_2 == "":
+        st.error("学号不能为空")
+        return 0
+    try:
+        int(student_id_2)
+    except ValueError:
+        st.error("学号必须为数字")
+        return 0
+    if int(student_id_2) not in table.index:
+        st.error("学号不存在")
+        return 0
+
+    contest_ratio /= 100
+    final_exam_ratio /= 100
+    contest_score = contest_result.loc[int(student_id_2), "单元测验"] * contest_ratio  # 单元测验成绩百分制
+    assignment_score = table.loc[int(student_id_2)].mean() * 10 * (1 - contest_ratio)  # 作业成绩十分制
+    final_exam_score = (60 - (contest_score + assignment_score) * (1 - final_exam_ratio)) / final_exam_ratio
+    final_exam_score = int(np.ceil(final_exam_score * 2)) // 2  # 向上取整到0.5分
+
+    return final_exam_score
+
+
 with tab_consult:
     student_id = st.text_input("学号", help="请输入学号", key="student_id")
     consult_button = st.button("查询", help="查询成绩")
@@ -88,29 +111,15 @@ with tab_compute:
     if version.parse(st.__version__) >= version.parse("1.29.0"):
         form_kw["border"] = False
     form = st.form(**form_kw)
-    student_id = form.text_input("学号", help="请输入学号", key="student_id_2")
+    student_id_2 = form.text_input("学号", help="请输入学号", key="student_id_2")
     contest_ratio = form.selectbox("单元测验占平时成绩百分比", [30, 20], index=1)
     final_exam_ratio = form.selectbox("期末考试占总成绩百分比", [70, 60], index=1)
     compute_button = form.form_submit_button("计算及格所需要的期末卷面最低分数")
 
-    if compute_button or student_id:
-        if student_id == "":
-            st.error("学号不能为空")
-        try:
-            int(student_id)
-        except ValueError:
-            st.error("学号必须为数字")
-        if int(student_id) not in table.index:
-            st.error("学号不存在")
-
-        contest_ratio /= 100
-        final_exam_ratio /= 100
-        contest_score = contest_result.loc[int(student_id), "单元测验"] * contest_ratio  # 单元测验成绩百分制
-        assignment_score = table.loc[int(student_id)].mean() * 10 * (1 - contest_ratio)  # 作业成绩十分制
-        final_exam_score = (60 - (contest_score + assignment_score) * (1 - final_exam_ratio)) / final_exam_ratio
-        final_exam_score = int(np.ceil(final_exam_score * 2)) // 2  # 向上取整到0.5分
-
-        st.markdown(f"及格所需要的期末卷面最低分数为**{final_exam_score}**分")
+    if compute_button or student_id_2:
+        final_exam_score = compute_final_exam_score(contest_ratio, final_exam_ratio)
+        if final_exam_score > 0:
+            st.markdown(f"及格所需要的期末卷面最低分数为**{final_exam_score}**分")
 
 # command to run:
 # nohup streamlit run webapp_2024.py --server.port 8502 > .logs/webapp_2024.log 2>&1 & echo $! > .logs/webapp_2024.pid
