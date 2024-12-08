d82072e722440cc83bb391256e820f1421fe0da5
WEN Hao <wenh06@gmail.com>
2024-12-08 11:18:38 +0800

    update the webapp for 2024 calculus

--- Calculus/webapp_2024.py
+++ Calculus/webapp_2024.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import streamlit as st
+from packaging import version
 
 st.set_page_config(page_title="2024秋微积分作业缺交情况查询", page_icon="📜", layout="centered")
 
@@ -22,6 +23,20 @@ def load_table():
     return table
 
 
+@st.cache_resource
+def load_contest_result():
+    # path = "./data/2024选课名单.csv"
+    contest_result = pd.read_csv(db_file)
+    contest_result["学号"] = contest_result["学号"].astype(int)
+    contest_result = contest_result.set_index("学号")
+    contest_result = contest_result[["10", "11"]]
+    contest_result["单元测验"] = contest_result.mean(axis=1)
+    contest_result = contest_result.drop(columns=["10", "11"])
+    return contest_result
+
+
+contest_result = load_contest_result()
+
 table = load_table()
 
 
@@ -38,7 +53,7 @@ elif db_file.stat().st_mtime > st.session_state["db_last_update_time"]:
 
 st.title("2024秋微积分作业缺交情况查询")
 
-student_id = st.text_input("学号", help="请输入学号", key="student_id")
+tab_consult, tab_compute = st.tabs(["作业缺交情况查询", "期末成绩计算"])
 
 
 def consult():
@@ -61,10 +76,41 @@ def consult():
         st.write(f"缺交第{', '.join(map(str, indices + 1))}次作业")
 
 
-consult_button = st.button("查询", help="查询成绩")
+with tab_consult:
+    student_id = st.text_input("学号", help="请输入学号", key="student_id")
+    consult_button = st.button("查询", help="查询成绩")
 
-if consult_button or student_id:
-    consult()
+    if consult_button or student_id:
+        consult()
+
+with tab_compute:
+    form_kw = dict(key="input_form")
+    if version.parse(st.__version__) >= version.parse("1.29.0"):
+        form_kw["border"] = False
+    form = st.form(**form_kw)
+    student_id = form.text_input("学号", help="请输入学号", key="student_id_2")
+    contest_ratio = form.selectbox("单元测验占平时成绩百分比", [30, 20], index=1)
+    final_exam_ratio = form.selectbox("期末考试占总成绩百分比", [70, 60], index=1)
+    compute_button = form.form_submit_button("计算及格所需要的期末卷面最低分数")
+
+    if compute_button or student_id:
+        if student_id == "":
+            st.error("学号不能为空")
+        try:
+            int(student_id)
+        except ValueError:
+            st.error("学号必须为数字")
+        if int(student_id) not in table.index:
+            st.error("学号不存在")
+
+        contest_ratio /= 100
+        final_exam_ratio /= 100
+        contest_score = contest_result.loc[int(student_id), "单元测验"] * contest_ratio  # 单元测验成绩百分制
+        assignment_score = table.loc[int(student_id)].mean() * 10 * (1 - contest_ratio)  # 作业成绩十分制
+        final_exam_score = (60 - (contest_score + assignment_score) * (1 - final_exam_ratio)) / final_exam_ratio
+        final_exam_score = int(np.ceil(final_exam_score * 2)) // 2  # 向上取整到0.5分
+
+        st.markdown(f"及格所需要的期末卷面最低分数为**{final_exam_score}**分")
 
 # command to run:
 # nohup streamlit run webapp_2024.py --server.port 8502 > .logs/webapp_2024.log 2>&1 & echo $! > .logs/webapp_2024.pid
